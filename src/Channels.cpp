5c8ea183721acd655a9822f3df9f01732a48ab88
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-03-09 12:56:07 +0100

    pusho

--- src/Channels.cpp
+++ src/Channels.cpp
@@ -2,7 +2,6 @@
 #include "Channels.h"
 
 namespace proto {
-
     const std::map<ChannelType, Channel> CHANNELS = {
             {GEOMETRY_CHANNEL,                 {"geometry",                "127.0.0.1", "5556"}},
             {REFEREE_CHANNEL,                  {"referee",                 "127.0.0.1", "5557"}},
@@ -13,10 +12,9 @@ namespace proto {
             {FEEDBACK_SECONDARY_CHANNEL,       {"feedback_yellow",         "127.0.0.1", "5562"}},
             {SETTINGS_PRIMARY_CHANNEL,         {"settings_primary",        "127.0.0.1", "5563"}},
             {SETTINGS_SECONDARY_CHANNEL,       {"settings_secondary",      "127.0.0.1", "5564"}},
-            {AI_SETTINGS_RECEIVE,              {"ai_settings_receive",     "...",       "0"}},
-            {AI_DRAW_SEND,                     {"ai_draw_send",            "...",       "0"}},
-            {INTERFACE_SETTINGS_SEND,          {"interface_settings_send", "...",       "0"}},
-            {INTERFACE_DRAW_RECEIVE,           {"interface_draw_receive",  "...",       "0"}}
+            {AI_SETTINGS_RECEIVE,              {"ai_settings_receive",     "127.0.0.1", "5565"}},
+            {AI_DRAW_SEND,                     {"ai_draw_send",            "127.0.0.1", "5566"}},
+            {INTERFACE_SETTINGS_SEND,          {"interface_settings_send", "127.0.0.1", "5567"}},
+            {INTERFACE_DRAW_RECEIVE,           {"interface_draw_receive",  "127.0.0.1", "5568"}}
     };
-
 } // proto
